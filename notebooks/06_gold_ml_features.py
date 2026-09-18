a4832d846795ed1b6d779d9045a2ee020a10f447
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:05:42 -0500

    Add ML features notebook scaffold

---
+++ notebooks/06_gold_ml_features.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 06_gold_ml_features notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 06_gold_ml_features")
