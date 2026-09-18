49bb3ecdd41b90bbaea68d3eb2a70ad13319ce48
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:06:10 -0500

    Add growth classifier notebook scaffold

---
+++ notebooks/11_growth_classifier.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 11_growth_classifier notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 11_growth_classifier")
