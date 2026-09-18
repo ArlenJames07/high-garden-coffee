fdc6e2ba3739e06e057b4eb097595939fa5a3c98
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:05:59 -0500

    Add batch forecasting notebook scaffold

---
+++ notebooks/09_batch_forecasting.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 09_batch_forecasting notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 09_batch_forecasting")
