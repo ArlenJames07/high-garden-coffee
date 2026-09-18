aac49db4fb6a9b5083a631aaaeebe802cbca5c50
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:05:30 -0500

    Add silver notebook scaffold

---
+++ notebooks/03_silver_transformation.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 03_silver_transformation notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 03_silver_transformation")
