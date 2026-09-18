ba40c1dfcee2a87a00dd10bd42504f3f2104e063
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:05:24 -0500

    Add bronze notebook scaffold

---
+++ notebooks/02_bronze_ingestion.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 02_bronze_ingestion notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 02_bronze_ingestion")
