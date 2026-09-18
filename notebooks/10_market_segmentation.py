9318369e53455d79235f21ef1e0f8ccdf56846a2
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:06:04 -0500

    Add market segmentation notebook scaffold

---
+++ notebooks/10_market_segmentation.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 10_market_segmentation notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 10_market_segmentation")
