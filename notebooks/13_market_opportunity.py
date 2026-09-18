ad33cae9c6580b56c48ff63cc60ce1c9c65314bb
Arlen James Mosquera Ruiz <150406790+ArlenJames07@users.noreply.github.com>
2026-09-18 01:06:21 -0500

    Add market opportunity notebook scaffold

---
+++ notebooks/13_market_opportunity.py
@@ -0,0 +1,3 @@
+# Databricks notebook source
+# TODO: Replace this scaffold with the completed 13_market_opportunity notebook exported from the workspace.
+print("TODO: sync completed Databricks notebook: 13_market_opportunity")
